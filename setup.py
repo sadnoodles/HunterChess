f01643a0f273be12756394dbd9ae8fdab3f639c7
sadnoodles <sadnoodles@gmail.com>
2014-02-24 19:32:55 +0800

    ..

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ from cx_Freeze import setup, Executable
 base = None
 if sys.platform == "win32":
     base = "Win32GUI"
-# scriptname=open('setup.txt','r').read().split('\\')[-1][:-1]
 scriptname="hunter_chess.py"
 setup(
         name = "HunterChess",
